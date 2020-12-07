07301a49d995c9de00ed92625263fe760f29fab2
Paulo S. Costa <pauloscosta5@gmail.com>
2020-12-07 11:50:29 -0800

    Double quote

--- python/matplotlib.py
+++ python/matplotlib.py
@@ -4,7 +4,7 @@
 ax.legend(loc="upper center", bbox_to_anchor=(0.5, 0), ncol=5)
 
 # Increase pixel density in notebooks
-%config InlineBackend.figure_format='retina'
+%config InlineBackend.figure_format="retina"
 
 # Remove vertial grid lines
 ax.xaxis.grid(False)
