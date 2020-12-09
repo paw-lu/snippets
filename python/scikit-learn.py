478c9ca71026659a5ccecdafbdfe3f7b45e23911
Paulo S. Costa <pauloscosta5@gmail.com>
2020-12-09 10:54:04 -0800

    [scikit-learn] split feature names into cont/cat

--- python/scikit-learn.py
+++ python/scikit-learn.py
@@ -63,11 +63,11 @@ def make_pipeline(
     return full_pipe
 
 
-def get_feature_names(
+def get_continuous_feature_names(
     pipeline: Pipeline, transformer_name: str, feature_names: List[str]
 ) -> List[str]:
     """
-    Get the names for the transformed features.
+    Get the names for the transformed continuous features.
 
     Parameters
     ----------
@@ -89,3 +89,44 @@ def get_feature_names(
         .named_transformers_[transformer_name]["impute"]
         .indicator_.features_
     ]
+
+
+def get_categorical_feature_names(
+    pipeline: Pipeline, transformer_name: str, feature_names: List[str]
+) -> List[str]:
+    """
+    Get the names of the transformed categorial features.
+
+    Parameters
+    ----------
+    pipeline : Pipeline
+        The full pipeline.
+    transformer_name : str
+        The name of the transformer in the pipeline.
+    feature_names : List[str]
+        The names of the features associated with the transformer_name.
+
+    Returns
+    -------
+    List[str]
+        A list of processed column names.
+    """
+    full_feature_names = feature_names + [
+        f"{feature_names[idx]}_missing"
+        for idx in pipeline["preprocess"]
+        .named_transformers_[transformer_name]["impute"]
+        .indicator_.features_
+    ]
+    feature_names_mapping = {
+        f"x{idx}": feature_name for idx, feature_name in enumerate(full_feature_names)
+    }
+    encoded_feature_names = (
+        pipeline["preprocess"]
+        .named_transformers_["cat_features"]["encode"]
+        .get_feature_names()
+    )
+    categorical_feature_names = []
+    for feature_name in encoded_feature_names:
+        prefix, name = feature_name.split("_", maxsplit=1)
+        categorical_feature_names.append(f"{feature_names_mapping[prefix]}_{name}")
+    return categorical_feature_names
