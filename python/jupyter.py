5e572ac273b7a8f58b518ea0b61c6d7ef978ac6f
Paulo S. Costa <pauloscosta5@gmail.com>
2020-12-09 16:49:43 -0800

    [jupyter] autoreload

---
+++ python/jupyter.py
@@ -0,0 +1,8 @@
+"""Snippets and useful magic for Jupyter and iPython."""
+
+# Increase pixel density in notebooks
+%config InlineBackend.figure_format="retina"
+
+# Reload imports
+%load_ext autoreload
+%autoreload 2
